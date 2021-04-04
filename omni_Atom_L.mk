12d79ddbfb5811e19b0fa3f3e6bc8facc9259b89
Matthias Leitl <a.dead.trousers@gmail.com>
2021-04-04 14:32:07 +0200

    Sync with Atom XL

--- omni_Atom_L.mk
+++ omni_Atom_L.mk
@@ -1,6 +1,9 @@
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
    
 # Inherit from Atom_XL device
 $(call inherit-product, device/Unihertz/Atom_L/device.mk)
@@ -10,11 +13,12 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 # If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/Unihertz/Atom_L/Atom_L-vendor.mk)
 
-# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
+# Files under $(LOCAL_PATH)/recovery/root/ get automatically copied into recovery
 # PRODUCT_COPY_FILES += device/Unihertz/Atom_L/prebuilt/Image.gz:kernel
 # PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
 
@@ -23,6 +27,8 @@ PRODUCT_NAME := omni_Atom_L
 PRODUCT_BRAND := Unihertz
 PRODUCT_MODEL := Atom_L
 PRODUCT_MANUFACTURER := Unihertz
+
+PRODUCT_BOARD := g63v71c2k_dfl_eea
 
 # Add fingerprint from Stock ROM build.prop
 BUILD_FINGERPRINT := "Unihertz/Atom_L_EEA/Atom_L:10/QP1A.190711.020/root.20200916.184140:user/release-keys"
