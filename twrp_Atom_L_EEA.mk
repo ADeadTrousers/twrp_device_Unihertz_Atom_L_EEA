23dcd69f91b8b13cab69fe0e01accb24ece4670e
Matthias Leitl <a.dead.trousers@gmail.com>
2022-05-01 12:38:56 +0200

    Change to AOSP build environment for TWRP 3.6+

--- twrp_Atom_L_EEA.mk
+++ twrp_Atom_L_EEA.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 #$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 # If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+#$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit from vendor
 $(call inherit-product-if-exists, vendor/Unihertz/Atom_L_EEA/Atom_L_EEA-vendor.mk)
@@ -22,7 +22,7 @@ $(call inherit-product-if-exists, vendor/Unihertz/Atom_L_EEA/Atom_L_EEA-vendor.m
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,device/Unihertz/Atom_LXL/recovery/root,recovery/root)
 
-PRODUCT_NAME := omni_Atom_L_EEA
+PRODUCT_NAME := twrp_Atom_L_EEA
 PRODUCT_DEVICE := Atom_L_EEA
 PRODUCT_BRAND := Unihertz
 PRODUCT_MODEL := Atom_L
