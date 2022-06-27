5421708b2e98ff843a7c06bc80703e256ac2f0ee
Matthias Leitl <a.dead.trousers@gmail.com>
2022-06-27 19:33:13 +0200

    Move parts to common repo

--- twrp_Atom_L_EEA.mk
+++ twrp_Atom_L_EEA.mk
@@ -1,26 +1,8 @@
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 # Inherit from common device
-#$(call inherit-product, device/Unihertz/Atom_LXL/deviceCommon.mk)
-
-# Another common config inclusion
-#$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-
-# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
-#$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, device/Unihertz/Atom_LXL/deviceCommon.mk)
 
 # Inherit from vendor
 $(call inherit-product-if-exists, vendor/Unihertz/Atom_L_EEA/Atom_L_EEA-vendor.mk)
-
-# Files under $(LOCAL_PATH)/recovery/root/ get automatically copied into recovery
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,device/Unihertz/Atom_LXL/recovery/root,recovery/root)
 
 PRODUCT_NAME := twrp_Atom_L_EEA
 PRODUCT_DEVICE := Atom_L_EEA
